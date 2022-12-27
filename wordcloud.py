e0402391abb779eab7127f3d9b08bdba7e6c2e89
EricaFinlay <erica.finlay@my.jcu.edu.au>
2022-12-28 09:08:43 +1000

    Edit list & function names and use set format

--- wordcloud.py
+++ wordcloud.py
@@ -2,24 +2,24 @@
 import operator
 import string
 
-VALUED_WORDS = ["word", "cloud", "punctuation"]
-FACTOR_TO_MULTIPLY_VALUE_WORDS_BY = 5
+VALUED_WORDS = ["word", "cloud", "punctuation", "string"]
+FACTOR_TO_MULTIPLY_VALUED_WORDS_BY = 5
 
 
 def word_cloud_logic(source_text, model="occurrence"):
-    """Produce a dictionary of words suitable for processing into a word cloud using an occurrence, value, length or
-    alphabetical based model."""
+    """Produce a dictionary of words suitable for processing into a word cloud using an occurrence, value, length
+    reversed or alphabetical based model."""
     processed_list = process_string(source_text)
     if model == "occurrence":
-        word_to_count = create_dictionary_based_on_word_occurrence(processed_list)
+        word_to_count = map_word_to_occurrences(processed_list)
     elif model == "value":
-        word_to_count = create_dictionary_based_on_valued_words(processed_list)
+        word_to_count = map_word_to_valued_words(processed_list)
     elif model == "length":
-        word_to_count = create_dictionary_based_on_word_length(processed_list)
+        word_to_count = map_word_to_word_length(processed_list)
     elif model == "reversed":
-        word_to_count = create_dictionary_based_on_reversal(processed_list)
+        word_to_count = create_reversed_words(processed_list)
     else:
-        word_to_count = create_dictionary_based_on_alphabetical_order(processed_list)
+        word_to_count = map_word_to_alphabetical_order(processed_list)
     word_to_count = sort_by_value(word_to_count)
     return word_to_count
 
@@ -52,58 +52,52 @@ def process_string(text_to_process):
     return processed_list
 
 
-def create_dictionary_based_on_word_occurrence(list_of_words):
+def map_word_to_occurrences(words):
     """Add list to dictionary with value based on occurrence."""
     word_to_count = {}
-    for word in list_of_words:
-        word_to_count[word] = list_of_words.count(word)
+    for word in set(words):
+        word_to_count[word] = words.count(word)
     return word_to_count
 
 
-def create_dictionary_based_on_valued_words(list_of_words):
+def map_word_to_valued_words(words):
     """Add list to dictionary with value based on nominated keywords."""
     word_to_count = {}
-    for word in list_of_words:
+    for word in set(words):
         word_to_count[word] = 1
         if word in VALUED_WORDS:
-            word_to_count[word] = (list_of_words.count(word) * FACTOR_TO_MULTIPLY_VALUE_WORDS_BY)
+            word_to_count[word] = (words.count(word) * FACTOR_TO_MULTIPLY_VALUED_WORDS_BY)
     return word_to_count
 
 
-def create_dictionary_based_on_word_length(list_of_words):
+def map_word_to_word_length(words):
     """Add list to dictionary with value based on length of word."""
     word_to_count = {}
-    for word in list_of_words:
+    for word in set(words):
         word_to_count[word] = len(word)
     return word_to_count
 
 
-def create_dictionary_based_on_reversal(list_of_words):
+def create_reversed_words(words):
+    """Add list to dictionary with words reversed (value of 1 applied to all)."""
     word_to_count = {}
-    for word in list_of_words:
+    for word in set(words):
         reversed_word = word[::-1]
         word_to_count[reversed_word] = 1
     return word_to_count
 
 
-def create_dictionary_based_on_alphabetical_order(list_of_words):
+def map_word_to_alphabetical_order(words):
     """Add list to dictionary with value based on alphabetical order."""
     word_to_count = {}
-    list_of_words.sort()
-    for word in list_of_words:
+    words.sort()
+    for word in set(words):
         word_to_count[word] = 1
     count = len(word_to_count.values())
     for key in word_to_count:
         word_to_count[key] = count
         count -= 1
     return word_to_count
-
-
-# def insert_name(dictionary, name):
-#     """Insert name of model into dictionary (will appear as the last item)."""
-#     maximum_value = (max(dictionary.values()) + 10)
-#     dictionary[name] = maximum_value
-#     return dictionary
 
 
 def sort_by_value(dictionary):
--- wordcloud_test.py
+++ wordcloud_test.py
@@ -1,19 +1,18 @@
 """Test word cloud tools."""
-# from wordcloud import insert_name
 from wordcloud import read_file
 from wordcloud import word_cloud_logic
 from visual_wordcloud import VisualWordCloud
 
-# TEST_STRING = "This is a test test string string string."
-TEST_FILE = "initial_text.txt"
-TEST_MODEL = "reversed"  # Model options are "occurrence", "value", "length" "reversed" and "alphabetical".
+TEST_STRING = "This is a test test string string string."
+# TEST_FILE = "initial_text.txt"
+TEST_MODEL = "alphabetical"  # Model options are "occurrence", "value", "length", "reversed" and "alphabetical".
 TEST_WORD_TO_OCCURRENCE = {"string": 3, "test": 2, "this": 1, "is": 1, "a": 1}
 
 
 def test_word_cloud():
-
-    # text = TEST_STRING
-    text = read_file(TEST_FILE)
+    """Test word cloud logic tools."""
+    text = TEST_STRING
+    # text = read_file(TEST_FILE)
     word_to_count = word_cloud_logic(text, TEST_MODEL)
     # word_to_count = TEST_WORD_TO_COUNT
     print(word_to_count)
@@ -21,7 +20,7 @@ def test_word_cloud():
     word_cloud = VisualWordCloud(word_to_count)
     for visual_word in word_cloud.visual_words:
         print(visual_word)
-    word_cloud.render_to_image(title=TEST_MODEL.title() + " Model")
+    word_cloud.render_to_image(title=f"{TEST_MODEL.title()} Model")
 
 
 if __name__ == '__main__':
